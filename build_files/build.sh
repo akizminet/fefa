decdcd20b4f9f1a44c6de1e866e3593a6c7cf2e4
Your Name <your_email@example.com>
2026-02-25 09:58:48 +0700

    feat: Update base image, configure custom and COPR repositories, install OpenVPN client and antigravity, and remove Firefox.

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,17 +8,12 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
+dnf5 config-manager addrepo --set=baseurl="https://us-central1-yum.pkg.dev/projects/antigravity-auto-updater-dev/antigravity-rpm" --id="antigravity-custom"
+dnf5 -y copr enable dsommers/openvpn3
+dnf5 config-manager setopt *-custom*.enabled=0
+dnf5 config-manager setopt *-custom*.gpgcheck=0
 # this installs a package from fedora repos
-dnf5 install -y tmux 
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+dnf5 install -y --setopt=*-custom*.enabled=1 openvpn3-client antigravity
+dnf5 remove -y firefox
+dnf5 -y copr disable dsommers/openvpn3
